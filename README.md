2aa551efca423b6928a54f54feedc28bdc2bf36e
MejanH <miza9ul@gmail.com>
2025-08-22 21:54:19 +0600

    feat: output noise cleaned audo files

---
+++ README.md
@@ -0,0 +1,8 @@
+# AI noise suppressor for microphone input, audio/video files, and more.
+
+The goal of this project is to provide a simple and effective way to reduce noise in audio streams using AI techniques.
+
+# Todos
+
+- [x] create a basic cmd interface and output noise suppressed audio with `deepfilternet`
+- [ ] try to noise suppress microphone input
---
+++ main.py
@@ -0,0 +1,53 @@
+from torch.utils.data import DataLoader
+from df import enhance, init_df
+from df.enhance import AudioDataset
+from df.model import ModelParams
+from df.io import load_audio, resample, save_audio
+from loguru import logger
+import torch
+import sys
+import glob
+import os
+import time
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <input_file> [output_file]")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2] if len(sys.argv) > 2 else None
+    output_dir = "."
+
+    torch.cuda.empty_cache()
+    model, df_state, suffix = init_df()
+
+    input_files = [input_file]
+    df_sr = ModelParams().sr
+    ds = AudioDataset(input_files, df_sr)
+    loader = DataLoader(ds, num_workers=2, pin_memory=True)
+
+    n_samples = len(ds)
+    for i, (file, audio, audio_sr) in enumerate(loader):
+        file = file[0]
+        audio = audio.squeeze(0)
+        progress = (i + 1) / n_samples * 100
+        t0 = time.time()
+        audio = enhance(model, df_state, audio, atten_lim_db=None)
+        t1 = time.time()
+        t_audio = audio.shape[-1] / df_sr
+        t = t1 - t0
+        rtf = t / t_audio
+        fn = os.path.basename(file)
+        p_str = f"{progress:2.0f}% | " if n_samples > 1 else ""
+        logger.info(
+            f"{p_str}Enhanced noisy audio file '{fn}' in {t:.2f}s (RT factor: {rtf:.3f})"
+        )
+        audio = resample(audio.to("cpu"), df_sr, audio_sr)
+        save_audio(
+            file, audio, sr=audio_sr, output_dir=output_dir, suffix=suffix, log=False
+        )
+
+
+if __name__ == "__main__":
+    main()
